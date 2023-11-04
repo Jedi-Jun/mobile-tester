e2e10885d605e09797f50f048b5e492e9caaa4de
jun_samsung_laptop <icevolt123@naver.com>
2023-11-05 03:12:08 +0900

    feat: visualViewport

--- app.js
+++ app.js
@@ -58,13 +58,39 @@ HTMLTemplate.section4 = (data = null) => {
   }
 };
 
-/* 5) IP address */
+/* 5) IP visibilitychange + focus + blur */
 HTMLTemplate.section5 = () => `
 <ol class='vis-wrapper'>
- <div class='vis-bottom'></div>
+  <p>=== Start to tab! ===</p>
+  <div class='vis-bottom'></div>
 </ol>
 `;
 
+/* 6) window.visualViewport & innerHeight */
+HTMLTemplate.section6 = () => `
+<div class='visual-wrapper'>
+  <div class='visual-header'>header</div>
+  <div class='visual-content'>
+    <div class='visual-content-innerHeight'>
+      innerHeight
+      <div></div>
+      </div>
+      <div class='visual-content-visualViewport'>
+      visualViewport
+      <div></div>
+    </div>
+  </div>
+</div>
+`;
+
+/* 7) Push Notification */
+HTMLTemplate.section7 = () => `
+<div>
+  Hello, Push Notification!
+</div>
+`;
+
+/* Main */
 window.addEventListener('load', () => {
   // (1) Set 100vh for Mobile Browser's height
   const vh = window.innerHeight * 0.01;
@@ -74,43 +100,11 @@ window.addEventListener('load', () => {
   appMain.innerHTML = HTMLTemplate.section1();
   getGeoLocation();
 
-  // (3) Tab detection
-  const addLog = text => {
-    const vis_wrapper = document.querySelector('.vis-wrapper');
-    const vis_bottom = document.querySelector('.vis-bottom');
-    if (!vis_wrapper) return;
-    if (document.hidden) {
-      const div = document.createElement('div');
-      div.classList.add('vis-time-wrapper');
-      const hr1 = document.createElement('hr');
-      const hr2 = document.createElement('hr');
-      const span = document.createElement('span');
-      span.classList.add('vis-time');
-      const options = {
-        timeZone: 'Asia/Seoul',
-        hour12: false,
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-      };
-      const time = new Intl.DateTimeFormat('en-US', options).format();
-      span.innerText = time;
-      div.append(hr1, span, hr2);
-      // div.append(span);
-      // div.append(hr);
-      vis_wrapper.insertBefore(div, vis_bottom);
-    }
+  // (3) Browser visibility detection
+  handleSection5();
 
-    const li = document.createElement('li');
-    li.innerText = text;
-    vis_wrapper.insertBefore(li, vis_bottom);
-    vis_bottom.scrollIntoView(false);
-  };
-  window.addEventListener('visibilitychange', () =>
-    addLog(`visibilitychange: ${document.visibilityState}`)
-  );
-  window.addEventListener('focus', () => addLog('Focus: user came in.'));
-  window.addEventListener('blur', () => addLog('Blur: user went out.'));
+  // (4) Get visualViewport & innerHeight by resize
+  handleSection6();
 });
 
 navButtons.forEach(btn => {
@@ -142,7 +136,7 @@ function createHTMLForSection2() {
 
 async function getIpInfo() {
   const URL = 'https://test.dongrim.site/api/v1/ip';
-    abortController = new AbortController();
+  abortController = new AbortController();
   const signal = abortController.signal;
 
   fetch(URL, { signal })
@@ -180,6 +174,91 @@ function getGeoLocation() {
   };
 
   navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
+function handleSection5() {
+  const queue = [
+    {
+      eventName: 'visibilitychange',
+      logMessage: `visibilitychange: ${document.visibilityState}`,
+    },
+    { eventName: 'focus', logMessage: 'Focus: user came in.' },
+    { eventName: 'blur', logMessage: 'Blur: user went out.' },
+  ];
+
+  queue.forEach(data => {
+    window.addEventListener(data.eventName, () => addLog(data.logMessage));
+  });
+
+  function addLog(text) {
+    const vis_wrapper = document.querySelector('.vis-wrapper');
+    const vis_bottom = document.querySelector('.vis-bottom');
+    if (!vis_wrapper) return;
+    if (document.hidden) {
+      const div = document.createElement('div');
+      div.classList.add('vis-time-wrapper');
+      const hr1 = document.createElement('hr');
+      const hr2 = document.createElement('hr');
+      const span = document.createElement('span');
+      span.classList.add('vis-time');
+      const options = {
+        timeZone: 'Asia/Seoul',
+        hour12: false,
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      };
+      const time = new Intl.DateTimeFormat('en-US', options).format();
+      span.innerText = time;
+      div.append(hr1, span, hr2);
+      vis_wrapper.insertBefore(div, vis_bottom);
+    }
+
+    const li = document.createElement('li');
+    li.innerText = text;
+    vis_wrapper.insertBefore(li, vis_bottom);
+    vis_bottom.scrollIntoView(false);
+  }
+}
+
+function handleSection6() {
+  window.addEventListener('resize', handleGetHeight1);
+  window.visualViewport.addEventListener('resize', handleGetHeight2);
+
+  appMain.addEventListener('click', () => {
+    const visualWrapper = document.querySelector('.visual-wrapper');
+    if (visualWrapper) {
+      handleGetHeight1();
+      handleGetHeight2();
+    }
+  });
+
+  function handleGetHeight1() {
+    const innerHeightElement = document.querySelector(
+      '.visual-content-innerHeight'
+    );
+    innerHeightElement.children[0].innerHTML = `
+      <span>window.innerHeight: ${window.innerHeight}</span>
+      <span>window.innerWidth: ${window.innerWidth}</span>
+    `;
+  }
+
+  function handleGetHeight2() {
+    const visualViewportElement = document.querySelector(
+      '.visual-content-visualViewport'
+    );
+    visualViewportElement.children[0].innerHTML = `
+      <span>height: ${window.visualViewport.height}</span>
+      <span>offsetLeft: ${window.visualViewport.offsetLeft}</span>
+      <span>offsetTop: ${window.visualViewport.offsetTop}</span>
+      <span>onresize: ${window.visualViewport.onresize}</span>
+      <span>onscroll: ${window.visualViewport.onscroll}</span>
+      <span>pageLeft: ${window.visualViewport.pageLeft}</span>
+      <span>pageTop: ${window.visualViewport.pageTop}</span>
+      <span>scale: ${window.visualViewport.scale}</span>
+      <span>width: ${window.visualViewport.width}</span>
+    `;
+  }
 }
 
 function handleSelectedEffect(ele) {
